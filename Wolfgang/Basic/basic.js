1718c1a71991079a245eb571a2ca0a264c6fc0ed
Wolfgang Figl <wolfgang.figl@me.com>
2019-10-29 10:50:13 +0100

    dfadfafda

---
+++ Wolfgang/Basic/basic.js
@@ -0,0 +1,7 @@
+var tablearray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (var _i = 0, tablearray_1 = tablearray; _i < tablearray_1.length; _i++) {
+    var a = tablearray_1[_i];
+    var b = 3;
+    var result = a * b;
+    console.log(a + "x" + b + "=" + result);
+}
---
+++ Wolfgang/Basic/basic.ts
@@ -0,0 +1,8 @@
+let tablearray: Array<number> = [1,2,3,4,5,6,7,8,9,10];
+
+for( let a of tablearray){
+
+	let b: number = 3;
+	let result: number = a*b;
+	console.log(a + "x" +b +"=" + result);
+}
